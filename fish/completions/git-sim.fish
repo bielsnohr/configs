dd3daaadd24d7b89c0ae185c0e93d9dcb81fb535
bielsnohr <matthew.bluteau@gmail.com>
2025-04-17 13:34:10 +0100

    Update fish config tracking for use with fisher

--- fish/completions/git-sim.fish
+++
@@ -1,1 +0,0 @@
-complete --command git-sim --no-files --arguments "(env _GIT_SIM_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) git-sim)" --condition "env _GIT_SIM_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) git-sim"
--- fish/config.fish
+++ fish/config.fish
@@ -6,12 +6,23 @@ if test -e $HOME/miniforge3/bin/conda
 end
 # <<< conda initialize <<<
 
+## Set vim keybindings
+set -g fish_key_bindings fish_vi_key_bindings
+
 ## Install Ruby Gems to ~/gems
 set --export GEM_HOME $HOME/gems
 set --export GEM_PATH $HOME/gems
 
 ## NPM
 set --export NPM_CONFIG_PREFIX ~/.npm-global
+
+## Torax
+# TODO put in a machine specific config
+set --export TORAX_QLKNN_MODEL_PATH $HOME/work/courses/deepmind_jax_workshop/torax_tutorial/fusion_surrogates/fusion_surrogates/models/qlknn_7_11.qlknn
+set --export JAX_COMPILATION_CACHE_DIR $HOME/.jax_cache
+set --export JAX_PERSISTENT_CACHE_MIN_ENTRY_SIZE_BYTES -1
+set --export JAX_PERSISTENT_CACHE_MIN_COMPILE_TIME_SECS 0.0
+set --export TORAX_GEOMETRY_DIR $HOME/work/courses/deepmind_jax_workshop/torax_tutorial/torax/torax/data/third_party/geo
 
 ## Global Path
 fish_add_path --global ~/bin ~/.npm-global/bin ~/.local/bin ~/gems/bin ~/.local/share/JetBrains/Toolbox/scripts
